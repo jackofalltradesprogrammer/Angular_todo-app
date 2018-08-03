161281bbdf49809e096141d4597497363458f13e
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2018-08-03 14:06:51 -0400

    Test returs all todos added to service

--- src/app/todo-data.service.spec.ts
+++ src/app/todo-data.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { TodoDataService } from './todo-data.service';
+import { Todo } from './todo';
 
 describe('TodoDataService', () => {
   beforeEach(() => {
@@ -19,5 +20,14 @@ describe('TodoDataService', () => {
       inject([TodoDataService], (service: TodoDataService) => {
         expect(service.getAllTodos()).toEqual([]);
     }));
+
+    it("should return all todos", 
+      inject([TodoDataService], (service: TodoDataService) => {
+        let todo1 = new Todo({title: "Hello 1", complete: false});
+        let todo2 = new Todo({title: "Hello 2", complete: true});
+        service.addTodo(todo1);
+        service.addTodo(todo2);
+        expect(service.getAllTodos()).toEqual([todo1, todo2]);
+      }));
   });
 });
