3fd471f06f64372b22a030ec43932d7f473fa6aa
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2018-08-04 23:09:50 -0400

    Add service dependency and view logic: functions and properties also

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
+// Import class so we can register it as dependency injection token
+import {TodoDataService} from './todo-data.service';
+import { Todo } from './todo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [TodoDataService]
 })
 export class AppComponent {
-  title = 'todo-app';
+
+  newTodo: Todo = new Todo();
+  
+  //Ask Angular DI system to inject the dependency
+  //associated with the dependency injection token 'TodoDataService'
+  //and assign it to a property called 'todoDataService'
+  constructor(private todoDataService: TodoDataService){}
+
+  // Service is now available as this.todoDataService
+
+  addTodo(){
+    this.todoDataService.addTodo(this.newTodo);
+  }
+
+  toggleTodoComplete(todo){
+    this.todoDataService.toggleTodoComplete(todo);
+  }
+
+  removeTodo(todo){
+    this.todoDataService.deleteTodoById(todo.id);
+  }
+
+  get todos(){
+    return this.todoDataService.getAllTodos();
+  }
 }
