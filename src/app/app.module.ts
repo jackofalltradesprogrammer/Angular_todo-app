96558d84987e10d014a0f45f7d7ac6a82208989e
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2018-08-08 22:10:31 -0400

    Add component to move the header to a child component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { TodoListHeaderComponent } from './todo-list-header/todo-list-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoListHeaderComponent
   ],
   imports: [
     BrowserModule, FormsModule
